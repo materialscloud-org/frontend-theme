b3b8bd7c2ccebcfcc30043786418e758c7499fec
Elsa Passaro <elsa.passaro@epfl.ch>
2021-09-21 14:15:13 +0200

    added breadcrumb, removed 'More' in jinja header

--- header/jinja/app/header.py
+++ header/jinja/app/header.py
@@ -6,6 +6,7 @@ mcloud_base_url = "https://www.materialscloud.org"
 urls = {
  "learn": "{}/learn".format(mcloud_base_url),
  "work": "{}/work".format(mcloud_base_url),
+ "tools": "{}/work/tools".format(mcloud_base_url),
  "discover": "{}/discover".format(mcloud_base_url),
  "explore": "{}/explore".format(mcloud_base_url),
  "archive": "{}/archive".format(mcloud_base_url),
@@ -14,10 +15,10 @@ urls = {
 # set the css class of your section to "active"
 css_classes = {
  "learn": "",
- "work": "",
+ "work": "active",
  "discover": "",
  "explore": "",
- "archive": "active",
+ "archive": "",
 }
 
 template_vars = dict(
